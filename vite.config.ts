9bf9e48c43335621f7a17b1843411898e275d4f0
victoria tonkovich <tonkovich.vika@gmail.com>
2026-07-23 18:06:17 +0300

    chore: add base configs

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: true,
+  },
+});
